93aa682901db70b0402ba7bbc00a711bcd0c0481
Biraj <biraj.pub@gmail.com>
2025-03-25 09:59:28 +0530

    fix: fix examples

--- examples/src/openai-responses.ts
+++ examples/src/openai-responses.ts
@@ -24,7 +24,7 @@ const obsy = new ObsyClient({
   projectId: env.OBSY_PROJECT_ID!,
 });
 
-// 2. instrument OpenAI and Pinecone clients
+// 2. instrument OpenAI client
 obsy.instrument(openai);
 
 // 3. enable tracing for each request
